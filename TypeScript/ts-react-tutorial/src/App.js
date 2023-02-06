5e8faf2a6c8511c5c42ec11691b055db64fcfda5
dawnheee <sherlock130815@gmail.com>
2023-02-06 23:10:31 +0900

    TS/상태관리

---
+++ TypeScript/ts-react-tutorial/src/App.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var Counter_1 = require("./Counter");
+var App = function () {
+    return <Counter_1["default"] />; //컴파일이 왜 이렇게 되지?? 
+};
+exports["default"] = App;
